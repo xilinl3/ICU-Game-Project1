d80f7867da3d62e5f63743f115432622d31c3cf7
ShaCoZha <jerry.zhang0825@gmail.com>
2024-09-23 07:11:50 -0700

    增加玩家跳跃功能和地面接触检测方程

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //暂时用来方便测试的 可以随时删掉
-    public float moveSpeed = 5f; 
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
     private Rigidbody2D rb;
+    private bool onGround;
 
     void Start()
     {
@@ -17,7 +19,27 @@ public class PlayerMovement : MonoBehaviour
     {
         float moveInput = Input.GetAxis("Horizontal");  
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
 }
 
